8f917351b4cf210586c32dce8502197439b76602
vysoko <sokolikvlad@yandex.ru>
2024-07-04 22:13:54 +0300

    внесены исправления по pr

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,43 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet weak var counterImage: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var refreshButton: UIButton!
-    @IBOutlet weak var historyView: UITextView!
+    // MARK: - IB Outlets
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
+    @IBOutlet weak private var refreshButton: UIButton!
+    @IBOutlet weak private var historyView: UITextView!
     
+    // MARK: - Private properties
+    private var countNumber: Int = 0
+    private let currentDate = Date.now.formatted()
     
+    // MARK: - View LC
     override func viewDidLoad() {
         super.viewDidLoad()
         showStartNumber()
         historySettings()
     }
     
-    var countNumber: Int = 0
-    let currentDate = Date.now.formatted()
-    
-    func historySettings () {
-        historyView.isEditable = false
-        historyView.isScrollEnabled = true
-    }
-    
-    func showStartNumber () {
-        counterImage.text = "Значение счетчика: 0"
-    }
-    
-    @IBAction func increaseCounter(sender: Any) {
+    // MARK: - IB Actions
+    @IBAction private func increaseCounter(sender: Any) {
         countNumber += 1
-        counterImage.text = "Значение счетчика: " + String(countNumber)
+        counterLabel.text = "Значение счетчика: " + String(countNumber)
             historyView.text += "\n \(currentDate) значение изменено на +1"
     }
     
-    @IBAction func decreaseCounter(_ sender: Any) {
+    @IBAction private func decreaseCounter(_ sender: Any) {
         if countNumber > 0 {
             countNumber -= 1
-            counterImage.text = "Значение счетчика: " + String(countNumber)
+            counterLabel.text = "Значение счетчика: " + String(countNumber)
             historyView.text += "\n \(currentDate) значение изменено на -1"
         }
         else
@@ -52,16 +46,26 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func setCounterToZero(_ sender: Any) {
+    @IBAction private func setCounterToZero(_ sender: Any) {
         if countNumber > 0 {
             countNumber = 0
-            counterImage.text = "Значение счетчика: " + String(countNumber)
+            counterLabel.text = "Значение счетчика: " + String(countNumber)
             historyView.text += "\n \(currentDate) значение сброшено"
         }
         else
         {
             historyView.text += "\n \(currentDate) значение уже равно 0, сброс невозможен"
         }
+    }
+    
+    // MARK: - Private Methods
+    private func historySettings () {
+        historyView.isEditable = false
+        historyView.isScrollEnabled = true
+    }
+    
+    private func showStartNumber () {
+        counterLabel.text = "Значение счетчика: 0"
     }
 }
     
